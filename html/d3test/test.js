98bda549f4e806edc595faf6e681eecea4d0c07d
axiomtutor <axiomtutor@protonmail.com>
2020-03-26 23:11:52 -0400

    new d3

--- html/d3test/test.js
+++ html/d3test/test.js
@@ -1,5 +1,5 @@
 (function() {
-  svg = d3.select('svg');
-  circ = svg.append('circle');
+  var svg = d3.select('svg');
+  var circ = svg.append('circle');
   circ.attr('r', 200).attr('cx', 480).attr('cy', 250);
 }());
